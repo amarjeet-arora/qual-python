5c0b70b38963af9e91f7ea77d6386e44fa811663
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-22 16:13:08 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,71 +1,14 @@
-from fastapi import FastAPI,status,Response,HTTPException
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
-
-#initialize the base data
-my_post=[{"title":"demo app","content":"post-1","id":1}]
-# initialize the fast API
-app= FastAPI()
-
-class Post(BaseModel):
-    title:str
-    content:str
-# add new record
-@app.post("/adduser",status_code=status.HTTP_201_CREATED)
-def create_post(newPost:Post):
-   
-    post_dict=newPost.dict()
-    print(post_dict)
-    post_dict['id']=randrange(0,10000000)
-    my_post.append(post_dict)
-    print(newPost)
-    print(newPost.model_dump())
-    return {"data":post_dict}
-# load all the records
-@app.get("/loadall")
-def loadAll():
-    return {"data": my_post}
-
-# helper function
-
-def findPost(id):
-    for p in my_post:
-        if p['id'] == id:
-            return p
-
-def findPost2(id):
-    for i, p in enumerate(my_post):
-        if p['id'] == id:
-            return i
-
-
-# search specific record
-@app.get("/loadpost/{uid}")
-def loadByPostId(uid:int,response:Response):
-    post=findPost(uid)
-    if not post:
-        response.status_code=status.HTTP_404_NOT_FOUND
-        print(post)
-        return {"Message":f"User not Found for :{uid}"}
-    else:
-        return {"data":post}
-
-# search specific record
-@app.get("/loadpost2/{uid}")
-def loadByPostId(uid:int,response:Response):
-    post=findPost(uid)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Given ID not found")
-    else:
-        return {"data":post}
-
-@app.delete("/deletepost/{id}")
-def deletePost(id:int):
+@app.put("/updatepost/{id}")
+def updatePost(id:int,post:Post):
+    print(post)
     post=findPost2(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Given ID not found")
-    else:
-        my_post.pop(post)
-        return {"Post deleted":post}
+    post_dict=post.model_dump()
+    post_dict['id'] =id
+    my_post[index]=post_dict
+    return {"data": post_dict}
 
+
+
+from operator import index
