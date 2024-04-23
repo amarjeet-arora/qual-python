72ae098e0625883bb5dc576418d439562d578578
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 11:54:04 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,20 +1,46 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import text
+from .database import Base
+
+class Student(Base):
+    __tablename__="STUDENT"
+
+    name=Column(String,primary_key=True,nullable=False)
+    sclass=Column(String,nullable=False)
+    section=Column(String,nullable=False)
+
+
+
+---
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL="sqlite:///./mytest.db"
-
-engine=create_engine(
-    SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":false}
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mytest.db"
+ 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-SessionLocal= sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
-Base= declarative_base()
+Base = declarative_base()
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+
+
+
+from pydantic import BaseModel
+
+
+
+class Student(BaseModel):
+    name: str
+    sclass: str
+    section: str
