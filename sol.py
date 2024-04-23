d88e9f4d66e027620fa43ca2b398029fbac283d8
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 14:56:09 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -13,3 +13,26 @@ def home2(name):
 
 if __name__== "__main__":
     app.run()
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    Welcome to Flask!!!!! {{mycity}} -- {{myname}}
+
+    <hr />
+    {% for x in range(10) %} {% if x%2 ==1 %}
+
+    <p>{{x}}</p>
+    {% endif %} {% endfor %}
+  </body>
+</html>
