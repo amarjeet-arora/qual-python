bf2280160814540dc36e19e86957a72181aa7d76
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 15:17:40 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,46 +1,27 @@
 
-from flask import Flask,redirect,url_for,render_template
-
-app = Flask(__name__)
-
-userdata=['admin','manager','qa']
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/<name>")
-def home2(name):
-    return render_template("index.html",users=userdata)
-
-if __name__== "__main__":
-    app.run()
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    Welcome to Flask!!!!! {{mycity}} -- {{myname}}
-
-    <hr />
-    {% for x in range(10) %} {% if x%2 ==1 %}
-
-    <p>{{x}}</p>
-    {% endif %} {% endfor %}
-
-    <hr>
-    <p> User Data</p>
-
-    {% for x in users %}
-     <p>{{x}}</p>
-     {% endfor %}
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{% block title %} {% endblock %}</title>
+</head>
+<body>
+    <h1>Amarjeets Website</h1>
+    {% block content %}
+     {% endblock %}
+</body>
 </html>
+
+
+
+
+
+
+{% extends "base.html"%}
+
+{% block title %} ABC Page {% endblock %}
+
+
+{% block content %} <h2>Welcome App</h2>{% endblock %}
