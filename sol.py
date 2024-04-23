3c85bc85f9028680df2621230e57f57391638155
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 12:34:27 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,72 +1,30 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.sql.expression import text
-from .database import Base
-
-class Student(Base):
-    __tablename__="STUDENT"
-
-    name=Column(String,primary_key=True,nullable=False)
-    sclass=Column(String,nullable=False)
-    section=Column(String,nullable=False)
-
-
-
----
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mytest.db"
- 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-
-
-
-from pydantic import BaseModel
-
-
-
-class Student(BaseModel):
-    name: str
-    sclass: str
-    section: str
-
-
-
-
-
-
-
 from operator import index
 from random import randrange
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Response, Depends
 from fastapi.params import Body
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
 from fastapi import status
 import sqlite3
-from . import models,schemas
-from .database import engine, get_db
+import models,schemas
+from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
- 
+
 app = FastAPI()
 
-@app.get("/testconn")
-def testdb(db: Session = Depends(get_db)):
-    return {"status":"success"}
 
+
+@app.get("/testconn")
+def loadAll(db: Session = Depends(get_db)):
+    return {"status": "success"}
+
+
+@app.post("/adduser", status_code=status.HTTP_201_CREATED)
+def createPost(student:schemas.Student,db:Session=Depends(get_db)):
+    #new_post=models.Student(name=student.name,sclass=student.sclass,section=student.section)
+    new_post=models.Student(**student.dict())
+    print(new_post)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data added": new_post}
