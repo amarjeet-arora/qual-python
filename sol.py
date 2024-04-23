8dbe34d6bf6536c82f327678de18961884ebf884
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 16:02:08 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,5 +1,5 @@
 
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request
 
 app = Flask(__name__)
 
@@ -9,59 +9,22 @@ userdata=['admin','manager','qa']
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST","GET"])
 def login():
-    return render_template("login.html")
+    if request.method=="POST":
+        user=request.form["nm"]
+        return redirect(url_for("home2",usr=user))
+    else:
+        return render_template("login.html")
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/<name>")
-def home2(name):
-    return render_template("index.html",users=userdata)
+@app.route("/<usr>")
+def home2(usr):
+    #return render_template("userdata.html")
+    return f"<h3>{usr}</h3>"
 
 if __name__== "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-{% extends "base.html"%}
-
-{% block title %} Login Page {% endblock %}
-
-
-{% block content %} <h2>User Login</h2>{% endblock %}
-
-
-
-
-
-{% extends "base.html"%}
-
-{% block title %} register Page {% endblock %}
-
-
-{% block content %} <h2>Registration App</h2>{% endblock %}
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{% block title %} {% endblock %}</title>
-</head>
-<body>
-    <h1>Amarjeets Website</h1>
-    {% block content %}
-     {% endblock %}
-</body>
-</html>
