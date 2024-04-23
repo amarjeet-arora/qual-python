b9fd9b9bae30c085fe4f2dc948e09ad4a512936b
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-04-23 13:06:34 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -9,15 +9,11 @@ import models,schemas
 from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
-
-
 
 @app.get("/testconn")
 def loadAll(db: Session = Depends(get_db)):
     return {"status": "success"}
-
 
 @app.post("/adduser", status_code=status.HTTP_201_CREATED)
 def createPost(student:schemas.Student,db:Session=Depends(get_db)):
@@ -28,3 +24,39 @@ def createPost(student:schemas.Student,db:Session=Depends(get_db)):
     db.commit()
     db.refresh(new_post)
     return {"data added": new_post}
+
+@app.get("/loadall")
+def loadAll(db:Session=Depends(get_db)):
+    posts=db.query(models.Student).all()
+    print(posts)
+    return {"data":posts}
+
+@app.get("/loadpost/{name}")
+def loadByPostId(name: str,db:Session=Depends(get_db)):
+    post=db.query(models.Student).filter(models.Student.name == name).first()
+    print(post)
+    return {"data": post}
+
+
+@app.delete("/deletepost/{name}")
+def deletePost(name:str,db:Session=Depends(get_db)):
+    post=db.query(models.Student).filter(models.Student.name == name)
+    if post.first() ==None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Given ID not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    
+    return {"data": "User deleted"}
+
+@app.put("/updatepost/{name}")
+def updatePost(name:str,newstudent:schemas.Student,db:Session=Depends(get_db)):
+    updated_post=db.query(models.Student).filter(models.Student.name == name)
+    student=updated_post.first()
+    if student ==None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Given ID not found")
+    updated_post.update(newstudent.dict(),synchronize_session=False)
+    db.commit()
+    return {"data":updated_post.first()}
+
+
+
